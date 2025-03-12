4234c9dfdb17562904bbbc8d5df394e7178d1f9c
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-13 00:27:30 +0800

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import DashboardPage from "./components/Dashboard";
+
+const App = () => {
+  return (
+    <div className="min-h-svh flex flex-col items-center justify-center">
+      <DashboardPage />
+    </div>
+  );
+};
+
+export default App;
