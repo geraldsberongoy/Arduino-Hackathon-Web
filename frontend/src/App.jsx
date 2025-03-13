3fcf1d84cb30f177f89ab09f8b08886bfbc95df4
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-13 22:38:45 +0800

    add routing for landing and dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import DashboardPage from "./components/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
   return (
-    <div className="min-h-svh flex flex-col items-center justify-center">
-      <DashboardPage />
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
     </div>
   );
 };
