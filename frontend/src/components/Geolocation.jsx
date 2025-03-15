d8350fbbe3dd3583d6084d3efe68f8e802f399ce
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-15 19:19:22 +0800

    feat: implement Geolocation component with Leaflet map and default marker icon

--- frontend/src/components/Geolocation.jsx
+++ frontend/src/components/Geolocation.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix for default marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 const Geolocation = () => {
-  return <div>Geolocation Content</div>;
+  const position = [14.61644, 121.05405]; // Default position (latitude, longitude)
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={30}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Geolocation;
