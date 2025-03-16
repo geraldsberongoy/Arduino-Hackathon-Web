bd1124308a00e08f5718cc8ccea3d22c2194a4b5
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-16 15:21:36 +0800

    feat: enhance User component layout and status display in Table

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,11 +1,81 @@
 import React from "react";
 import Table from "./ui/Table";
+import { SlidersHorizontal } from "lucide-react";
 
 const User = () => {
   return (
-    <div className="h-screen w-full flex flex-col">
-      <h1 className="header1">User</h1>
+    <div className="h-screen w-full flex flex-col px-5 py-2">
+      <h1 className="text-5xl font-bold font-['IBM_Plex_Sans'] text-[#30343f]">
+        User
+      </h1>
+      <div className=" justify-start text-[#fc6791] text-base font-normal font-['IBM_Plex_Sans']">
+        Lorem ipsum dolor sit amet
+      </div>
+      <div className="w-full flex items-center justify-end pt-10 pb-2">
+        <div className="w-[300px] h-10 px-5 bg-white rounded-[50px] outline-1 outline-offset-[-1px] outline-[#7a7a7a] inline-flex justify-start items-center gap-[3px] overflow-hidden">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full h-full bg-transparent text-[#7a7a7a] text-base font-normal font-['IBM_Plex_Sans'] outline-none"
+          />
+          <SlidersHorizontal size={20} color="#7a7a7a" />
+        </div>
+      </div>
+
       <Table />
+
+      <div className="w-full h-10 flex px-5 mt-2">
+        <div className="flex items-center gap-2 w-full">
+          <div className="w-5 h-5 bg-[#33bc85] rounded-full shadow-[2px_2px_4px_0px_rgba(255,255,255,1.00)]" />
+          <div className="w-full justify-center">
+            <span class="text-[#30343f] text-lg font-bold font-['IBM_Plex_Sans']">
+              Total Safe Users
+            </span>
+            <span class="text-[#30343f] text-lg font-normal font-['IBM_Plex_Sans']">
+              {" "}
+              = X
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3 w-full">
+          <div className="w-5 h-5 bg-[#ff9911] rounded-full shadow-[2px_2px_4px_0px_rgba(255,255,255,1.00)]" />
+          <div className="w-full justify-center">
+            <span class="text-[#30343f] text-lg font-bold font-['IBM_Plex_Sans']">
+              Total Warning Users
+            </span>
+            <span class="text-[#30343f] text-lg font-normal font-['IBM_Plex_Sans']">
+              {" "}
+              = X
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3 w-full">
+          <div className="w-5 h-5 bg-[#fb4f4f] rounded-full shadow-[2px_2px_4px_0px_rgba(255,255,255,1.00)]" />{" "}
+          <div className="w-full justify-center">
+            <span class="text-[#30343f] text-lg font-bold font-['IBM_Plex_Sans']">
+              Total Critical Users
+            </span>
+            <span class="text-[#30343f] text-lg font-normal font-['IBM_Plex_Sans']">
+              {" "}
+              = X
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3 w-full">
+          <div className="w-full justify-center">
+            <span class="text-[#30343f] text-lg font-bold font-['IBM_Plex_Sans']">
+              Last System Update
+            </span>
+            <span class="text-[#30343f] text-lg font-normal font-['IBM_Plex_Sans']">
+              {" "}
+              = 7:31PM
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/ui/Table.jsx
+++ frontend/src/components/ui/Table.jsx
@@ -5,7 +5,7 @@ const users = [
     name: "Hart Hagerty",
     location: "United States",
     temperatureLevel: "Normal",
-    status: "Active",
+    status: "Safe",
     lastUpdate: "2025-03-15",
     avatar: "https://img.daisyui.com/images/profile/demo/2@94.webp",
   },
@@ -13,7 +13,7 @@ const users = [
     name: "Brice Swyre",
     location: "China",
     temperatureLevel: "High",
-    status: "Inactive",
+    status: "Warning",
     lastUpdate: "2025-03-14",
     avatar: "https://img.daisyui.com/images/profile/demo/3@94.webp",
   },
@@ -21,7 +21,7 @@ const users = [
     name: "Marjy Ferencz",
     location: "Russia",
     temperatureLevel: "Low",
-    status: "Active",
+    status: "Safe",
     lastUpdate: "2025-03-13",
     avatar: "https://img.daisyui.com/images/profile/demo/4@94.webp",
   },
@@ -29,19 +29,64 @@ const users = [
     name: "Yancy Tear",
     location: "Brazil",
     temperatureLevel: "Normal",
-    status: "Active",
+    status: "Critical",
+    lastUpdate: "2025-03-12",
+    avatar: "https://img.daisyui.com/images/profile/demo/5@94.webp",
+  },
+  {
+    name: "Hart Hagerty",
+    location: "United States",
+    temperatureLevel: "Normal",
+    status: "Safe",
+    lastUpdate: "2025-03-15",
+    avatar: "https://img.daisyui.com/images/profile/demo/2@94.webp",
+  },
+  {
+    name: "Brice Swyre",
+    location: "China",
+    temperatureLevel: "High",
+    status: "Warning",
+    lastUpdate: "2025-03-14",
+    avatar: "https://img.daisyui.com/images/profile/demo/3@94.webp",
+  },
+  {
+    name: "Marjy Ferencz",
+    location: "Russia",
+    temperatureLevel: "Low",
+    status: "Safe",
+    lastUpdate: "2025-03-13",
+    avatar: "https://img.daisyui.com/images/profile/demo/4@94.webp",
+  },
+  {
+    name: "Yancy Tear",
+    location: "Brazil",
+    temperatureLevel: "Normal",
+    status: "Critical",
     lastUpdate: "2025-03-12",
     avatar: "https://img.daisyui.com/images/profile/demo/5@94.webp",
   },
 ];
 
 const Table = () => {
+  const getStatusColor = (status) => {
+    switch (status.toLowerCase()) {
+      case "safe":
+        return "bg-[#33bc85]/50 ";
+      case "warning":
+        return "bg-[#ff9911]/50";
+      case "critical":
+        return "bg-[#fb4f4f]/50";
+      default:
+        return "";
+    }
+  };
+
   return (
-    <div className="flex-1 w-full overflow-x-auto p-5 ">
+    <div className="flex-1 w-full overflow-x-auto border rounded-2xl border-base-300 text-black bg-white/60">
       <table className="table">
         {/* head */}
         <thead>
-          <tr>
+          <tr className="bg-white text-black">
             <th>Name</th>
             <th>Location</th>
             <th>Temperature Level</th>
@@ -51,7 +96,10 @@ const Table = () => {
         </thead>
         <tbody>
           {users.map((user, index) => (
-            <tr key={index} className="hover:bg-base-300">
+            <tr
+              key={index}
+              className={`${getStatusColor(user.status)} outline-1`}
+            >
               <td>
                 <div className="flex items-center gap-3">
                   <div className="avatar">
