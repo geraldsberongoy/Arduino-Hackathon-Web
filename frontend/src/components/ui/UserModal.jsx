293ad5727995d8d5b3c17d3a6ad3781456db0d73
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-03-17 18:23:29 +0800

    feat: replace close button SVG with Lucide icon in UserModal component

--- frontend/src/components/ui/UserModal.jsx
+++ frontend/src/components/ui/UserModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { X } from "lucide-react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -65,23 +66,10 @@ const UserModal = ({ user, onClose }) => {
       <div className="bg-white py-8 px-12 rounded-2xl shadow-2xl relative w-2/3 flex">
         <div className="w-1/2 flex flex-col items-center">
           <button
-            className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+            className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 cursor-pointer"
             onClick={onClose}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+            <X size={24} />
           </button>
           <img
             src={user.avatar}
